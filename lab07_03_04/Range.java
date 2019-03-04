4687a3c184bb8e7f0dfe8e5730b5c8a0a095edd6
Sir <antoinoky@gmail.com>
2019-03-04 13:04:14 +0100

    created

---
+++ lab07_03_04/Range.java
@@ -0,0 +1,24 @@
+package lab07_03_04;
+
+import java.util.Iterator;
+
+public class Range implements Iterable<Integer> {
+
+    private final int start, end;
+
+    // ranges from start (inclusive) to end (exclusive)
+    public Range(int start, int end) {
+	// completare
+    }
+
+    // ranges from 0 (inclusive) to end (exclusive)
+    public Range(int end) {
+        this
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+	// completare usando la classe RangeIterator
+    }
+
+}
---
+++ lab07_03_04/RangeIterator.java
@@ -0,0 +1,11 @@
+package lab07_03_04;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+// objects which iterate over intances of Range
+class RangeIterator implements Iterator<Integer> {
+
+    // completare
+    
+}
